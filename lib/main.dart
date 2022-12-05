86d0aea64f8d8668b872a4455547f62e18754a33
Mehrdad08 <moradi.mehrdad001@gmail.com>
2022-12-05 09:37:50 +0330

    Start Project

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Application());
+}
+
+
+class Application extends StatefulWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  State<Application> createState() => _ApplicationState();
+}
+
+class _ApplicationState extends State<Application> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
