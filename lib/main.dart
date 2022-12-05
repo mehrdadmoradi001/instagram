493e074585137faf77aa4b4287d04bda369e994a
Mehrdad08 <moradi.mehrdad001@gmail.com>
2022-12-05 13:32:43 +0330

    1- Adding widget Stack - property Position - property AlignmentDirectional 2- Adding Images 3- Create Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,61 @@ void main() {
   runApp(const Application());
 }
 
-
-class Application extends StatefulWidget {
+class Application extends StatelessWidget {
   const Application({Key? key}) : super(key: key);
 
   @override
-  State<Application> createState() => _ApplicationState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
 }
 
-class _ApplicationState extends State<Application> {
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('images/pattern1.png'),
+          repeat: ImageRepeat.repeat,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          alignment: AlignmentDirectional.bottomCenter,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 80),
+              child: Center(
+                child: Image(
+                  image: AssetImage('images/logo_splash.png'),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 30,
+              child: Column(
+                children: [
+                  Text(
+                    'from',
+                    style: TextStyle(fontSize: 18, color: Colors.grey),
+                  ),
+                  Text(
+                    'ExpertFlutter',
+                    style: TextStyle(fontSize: 18, color: Colors.blue),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
