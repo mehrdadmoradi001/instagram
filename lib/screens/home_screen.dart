b985efcd26150f13cc1c8abaf5081ed45c0f74b8
MehrdadMoradi001 <moradi.mehrdad001@gmail.com>
2022-12-24 14:51:22 +0330

    1- Adding Library dotted_border 2- Using Library dotted_border 3-

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:instagram/data/constants/constant_colors.dart';
+import 'package:dotted_border/dotted_border.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,26 +11,51 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: deepColor,
       body: SafeArea(
         child: Center(
-          child: Container(
-            width: 64,
-            height: 64,
-            decoration: BoxDecoration(
-              color: whiteColor,
-              borderRadius: BorderRadius.circular(17),
+          child: _getStoryBox(),
+        ),
+      ),
+    );
+  }
+
+  Widget _getStoryBox() {
+    return DottedBorder(
+          borderType: BorderType.RRect,
+          radius: Radius.circular(17),
+          padding: EdgeInsets.all(4),
+          color: pinkColor,
+          dashPattern: [50, 10],
+          strokeWidth: 2,
+          child: ClipRRect(
+            borderRadius: BorderRadius.all(
+              Radius.circular(15),
             ),
-            child: Padding(
-              padding: EdgeInsets.all(2),
-              child: Container(
-                width: 60,
-                height: 60,
-                decoration: BoxDecoration(
-                  color: deepColor,
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Image.asset('images/icon_plus.png'),
-              ),
+            child: Container(
+              height: 58,
+              width: 58,
+              child: Image.asset('images/profile.png'),
             ),
           ),
+        );
+  }
+
+  Widget _getAddStoryBox() {
+    return Container(
+      width: 64,
+      height: 64,
+      decoration: BoxDecoration(
+        color: whiteColor,
+        borderRadius: BorderRadius.circular(17),
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(2),
+        child: Container(
+          width: 60,
+          height: 60,
+          decoration: BoxDecoration(
+            color: deepColor,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Image.asset('images/icon_plus.png'),
         ),
       ),
     );
