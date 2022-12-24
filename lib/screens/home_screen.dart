d61c3bd448b7983aac40e12d0afca17f9dcab1f1
MehrdadMoradi001 <moradi.mehrdad001@gmail.com>
2022-12-24 13:45:10 +0330

    1- Adding one item Story 2- using Image.asset methode

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,6 +8,30 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: deepColor,
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            width: 64,
+            height: 64,
+            decoration: BoxDecoration(
+              color: whiteColor,
+              borderRadius: BorderRadius.circular(17),
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(2),
+              child: Container(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: deepColor,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Image.asset('images/icon_plus.png'),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
