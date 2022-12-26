03941b4d965a0f257592a6c6fca411034d2acfd6
MehrdadMoradi001 <moradi.mehrdad001@gmail.com>
2022-12-26 21:21:57 +0330

    1- Creating GridView

--- lib/screens/share_bottomsheet.dart
+++ lib/screens/share_bottomsheet.dart
@@ -14,17 +14,29 @@ class ShareBottomSheet extends StatelessWidget {
       ),
       child: BackdropFilter(
         filter: ImageFilter.blur(sigmaX: 40.0, sigmaY: 40.0),
-        child: Container(
-          height: 300,
-          child: Column(
-            children: [
-              Text('tessss'),
-              Text('tessss'),
-              Text('tessss'),
-              Text('tessss'),
-              Text('tessss'),
-            ],
+        child: Padding(
+          padding: EdgeInsets.all(40),
+          child: Container(
+            color: Color.fromRGBO(255, 255, 255, 0.09),
+            height: 300,
+            child: _getContent(),
           ),
+        ),
+      ),
+    );
+  }
+
+  GridView _getContent() {
+    return GridView.builder(
+      itemCount: 30,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 4,
+        mainAxisSpacing: 20,
+        crossAxisSpacing: 20
+      ),
+      itemBuilder: (BuildContext context, int index) => Container(  //
+        child: Container(
+          color: Colors.yellow,
         ),
       ),
     );
