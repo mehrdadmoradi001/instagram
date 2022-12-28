fd0a48d1b4ccec89ba3148c0fbdbd2e58957831e
MehrdadMoradi001 <moradi.mehrdad001@gmail.com>
2022-12-28 20:36:56 +0330

    1- Creating Search Box Ui

--- lib/screens/share_bottomsheet.dart
+++ lib/screens/share_bottomsheet.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:instagram/data/constants/constant_colors.dart';
 
 class ShareBottomSheet extends StatelessWidget {
   const ShareBottomSheet({this.controller, Key? key}) : super(key: key);
@@ -16,7 +17,7 @@ class ShareBottomSheet extends StatelessWidget {
       child: BackdropFilter(
         filter: ImageFilter.blur(sigmaX: 40.0, sigmaY: 40.0),
         child: Padding(
-          padding: EdgeInsets.all(40),
+          padding: EdgeInsets.symmetric(horizontal: 40),
           child: Container(
             color: Color.fromRGBO(255, 255, 255, 0.001),
             //height: 300,
@@ -47,6 +48,55 @@ class ShareBottomSheet extends StatelessWidget {
     return CustomScrollView(
       controller: controller,
       slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+            children: [
+              Container(
+                height: 5,
+                width: 67,
+                margin: EdgeInsets.only(top: 10, bottom: 22),
+                decoration: BoxDecoration(
+                  color: whiteColor,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(100),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Share',
+                    style: TextStyle(
+                        fontFamily: 'GB', fontSize: 20, color: whiteColor),
+                  ),
+                  Image.asset('images/icon_share_bottomsheet.png'),
+                ],
+              ),
+              SizedBox(height: 32),
+              Container(
+                height: 46,
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(255, 255, 255, 0.4),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(13),
+                  ),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    children: [
+                      Image.asset('images/icon_search.png'),
+                      SizedBox(width: 8),
+                      Expanded(child: TextField()),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 32),
+            ],
+          ),
+        ),
         SliverGrid(
           delegate: SliverChildBuilderDelegate(
             (context, index) => Container(
@@ -54,10 +104,7 @@ class ShareBottomSheet extends StatelessWidget {
             ),
           ),
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 4,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20
-          ),
+              crossAxisCount: 4, crossAxisSpacing: 20, mainAxisSpacing: 20),
         ),
       ],
     );
