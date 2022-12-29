843e0957d5d81a83a66c4904c3cd9b66fb525fdf
MehrdadMoradi001 <moradi.mehrdad001@gmail.com>
2022-12-29 20:45:53 +0330

    1- Creating a list category for the start screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -11,36 +11,74 @@ class SearchScreen extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            Container(
-              height: 46,
-              margin: EdgeInsets.only(left: 18,right: 18,top: 12),
-              decoration: BoxDecoration(
-                color: backSearchBox,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(13),
-                ),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                child: Row(
-                  children: [
-                    Image.asset('images/icon_search.png'),
-                    SizedBox(width: 8),
-                    Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Search User',
-                          hintStyle: TextStyle(color: whiteColor),
-                          enabledBorder: InputBorder.none,
-                          focusedBorder: InputBorder.none,
-                        ),
-                      ),
-                    ),
-                    Image.asset('images/icon_scan.png')
-                  ],
+            _getSearchScreen(),
+            _getStoryList(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _getStoryList() {
+    return Container(
+      height: 23,
+      margin: EdgeInsets.only(top: 20, bottom: 20),
+      child: ListView.builder(
+        itemCount: 10,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) => Container(
+          margin: EdgeInsets.symmetric(horizontal: 16),
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 18, vertical: 4),
+              child: Text(
+                'Mehrdad $index',
+                style: TextStyle(
+                  fontSize: 12,
+                  color: whiteColor,
+                  fontFamily: 'GM',
                 ),
               ),
             ),
+          ),
+          decoration: BoxDecoration(
+            color: backSearchBox,
+            borderRadius: BorderRadius.all(
+              Radius.circular(8),
+            ),
+          ),
+        ), //
+      ),
+    );
+  }
+
+  Widget _getSearchScreen() {
+    return Container(
+      height: 46,
+      margin: EdgeInsets.only(left: 18, right: 18, top: 12),
+      decoration: BoxDecoration(
+        color: backSearchBox,
+        borderRadius: BorderRadius.all(
+          Radius.circular(13),
+        ),
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Row(
+          children: [
+            Image.asset('images/icon_search.png'),
+            SizedBox(width: 8),
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search User',
+                  hintStyle: TextStyle(color: whiteColor),
+                  enabledBorder: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                ),
+              ),
+            ),
+            Image.asset('images/icon_scan.png')
           ],
         ),
       ),
