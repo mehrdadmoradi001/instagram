36c5daf997ed5956f5ccc1a6c9eecf126cd4b885
MehrdadMoradi001 <moradi.mehrdad001@gmail.com>
2022-12-29 15:22:14 +0330

    1- Using SliverPadding for Button Share

--- lib/screens/share_bottomsheet.dart
+++ lib/screens/share_bottomsheet.dart
@@ -21,14 +21,14 @@ class ShareBottomSheet extends StatelessWidget {
           child: Container(
             color: Color.fromRGBO(255, 255, 255, 0.001),
             //height: 300,
-            child: _getContent(),
+            child: _getContent(context),
           ),
         ),
       ),
     );
   }
 
-  Widget _getContent() {
+  Widget _getContent(BuildContext context) {
     // return GridView.builder(
     //   controller: controller,
     //   itemCount: 100,
@@ -45,76 +45,97 @@ class ShareBottomSheet extends StatelessWidget {
     //     ),
     //   ),
     // );
-    return CustomScrollView(
-      controller: controller,
-      slivers: [
-        SliverToBoxAdapter(
-          child: Column(
-            children: [
-              Container(
-                height: 5,
-                width: 67,
-                margin: EdgeInsets.only(top: 10, bottom: 22),
-                decoration: BoxDecoration(
-                  color: whiteColor,
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(100),
-                  ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Stack(
+      alignment: AlignmentDirectional.bottomCenter,
+      children: [
+        CustomScrollView(
+          controller: controller,
+          slivers: [
+            SliverToBoxAdapter(
+              child: Column(
                 children: [
-                  Text(
-                    'Share',
-                    style: TextStyle(
-                        fontFamily: 'GB', fontSize: 20, color: whiteColor),
-                  ),
-                  Image.asset('images/icon_share_bottomsheet.png'),
-                ],
-              ),
-              SizedBox(height: 32),
-              Container(
-                height: 46,
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(255, 255, 255, 0.4),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(13),
-                  ),
-                ),
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10),
-                  child: Row(
-                    children: [
-                      Image.asset('images/icon_search.png'),
-                      SizedBox(width: 8),
-                      Expanded(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            hintText: 'Search User',
-                            enabledBorder: InputBorder.none,
-                            focusedBorder: InputBorder.none,
-                          ),
-                        ),
+                  Container(
+                    height: 5,
+                    width: 67,
+                    margin: EdgeInsets.only(top: 10, bottom: 22),
+                    decoration: BoxDecoration(
+                      color: whiteColor,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(100),
                       ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Share',
+                        style: TextStyle(
+                            fontFamily: 'GB', fontSize: 20, color: whiteColor),
+                      ),
+                      Image.asset('images/icon_share_bottomsheet.png'),
                     ],
                   ),
-                ),
+                  SizedBox(height: 32),
+                  Container(
+                    height: 46,
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(255, 255, 255, 0.4),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(13),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Row(
+                        children: [
+                          Image.asset('images/icon_search.png'),
+                          SizedBox(width: 8),
+                          Expanded(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: 'Search User',
+                                enabledBorder: InputBorder.none,
+                                focusedBorder: InputBorder.none,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 32),
+                ],
               ),
-              SizedBox(height: 32),
-            ],
-          ),
+            ),
+            SliverGrid(
+              delegate: SliverChildBuilderDelegate(
+                childCount: 60,
+                (context, index) => _getItemGrid(),
+              ),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount( //
+                mainAxisExtent: 110,
+                crossAxisCount: 4,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 10,
+              ),
+            ),
+            SliverPadding(
+              padding: EdgeInsets.only(top: 95),
+            ),
+          ],
         ),
-        SliverGrid(
-          delegate: SliverChildBuilderDelegate(
-            childCount: 100,
-            (context, index) => _getItemGrid(),
-          ),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            mainAxisExtent: 110,
-            crossAxisCount: 4,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 10,
+        Positioned(
+          bottom: 47,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 45, vertical: 13),
+              child: Text(
+                'Share',
+                style: TextStyle(fontFamily: 'GB', fontSize: 16),
+              ),
+            ),
           ),
         ),
       ],
