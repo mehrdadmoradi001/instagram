698ebac3627261f02081cf61a55b6fa46d870a8b
MehrdadMoradi001 <moradi.mehrdad001@gmail.com>
2023-01-02 02:28:04 +0330

    1- changing GridView to SliverGrid and changing properties childrenDelegate to delegate & gridDelegate  to gridDelegate
    2- Using sliver of property SliverPadding

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -10,11 +10,10 @@ class SearchScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: deepColor,
       body: SafeArea(
-        child: Column(
-          children: [
+        child: CustomScrollView(
+          slivers: [
             _getSearchBox(),
             _getCategoryList(),
-            SizedBox(height: 20),
             _getStaggeredGridView(),
           ],
         ),
@@ -23,24 +22,10 @@ class SearchScreen extends StatelessWidget {
   }
 
   Widget _getStaggeredGridView() {
-    return Container(
-      height: 600,
-      width: 1200,
-      child: GridView.custom(
-        gridDelegate: SliverQuiltedGridDelegate(
-          crossAxisCount: 3,
-          mainAxisSpacing: 10,
-          crossAxisSpacing: 10,
-          repeatPattern: QuiltedGridRepeatPattern.inverted,
-          pattern: [
-            QuiltedGridTile(2, 1),
-            QuiltedGridTile(2, 2),
-            QuiltedGridTile(1, 1),
-            QuiltedGridTile(1, 1),
-            QuiltedGridTile(1, 1),
-          ],
-        ),
-        childrenDelegate: SliverChildBuilderDelegate( //
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16), //
+      sliver: SliverGrid(
+        delegate: SliverChildBuilderDelegate(
           childCount: 10,
           (context, index) => ClipRRect(
             borderRadius: BorderRadius.all(
@@ -54,71 +39,88 @@ class SearchScreen extends StatelessWidget {
             ),
           ),
         ),
+        gridDelegate: SliverQuiltedGridDelegate(
+          crossAxisCount: 3,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+          repeatPattern: QuiltedGridRepeatPattern.inverted,
+          pattern: [
+            QuiltedGridTile(2, 1),
+            QuiltedGridTile(2, 2),
+            QuiltedGridTile(1, 1),
+            QuiltedGridTile(1, 1),
+            QuiltedGridTile(1, 1),
+          ],
+        ),
       ),
     );
   }
 
   Widget _getCategoryList() {
-    return Container(
-      height: 23,
-      margin: EdgeInsets.only(top: 20, bottom: 20),
-      child: ListView.builder(
-        itemCount: 10,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) => Container(
-          margin: EdgeInsets.symmetric(horizontal: 16),
-          child: Center(
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 18, vertical: 4),
-              child: Text(
-                'Mehrdad $index',
-                style: TextStyle(
-                  fontSize: 12,
-                  color: whiteColor,
-                  fontFamily: 'GM',
+    return SliverToBoxAdapter(
+      child: Container(
+        height: 23,
+        margin: EdgeInsets.only(top: 20, bottom: 20),
+        child: ListView.builder(
+          itemCount: 10,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) => Container(
+            margin: EdgeInsets.symmetric(horizontal: 16),
+            child: Center(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 18, vertical: 4),
+                child: Text(
+                  'Mehrdad $index',
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: whiteColor,
+                    fontFamily: 'GM',
+                  ),
                 ),
               ),
             ),
-          ),
-          decoration: BoxDecoration(
-            color: backSearchBox,
-            borderRadius: BorderRadius.all(
-              Radius.circular(8),
+            decoration: BoxDecoration(
+              color: backSearchBox,
+              borderRadius: BorderRadius.all(
+                Radius.circular(8),
+              ),
             ),
-          ),
-        ), //
+          ), //
+        ),
       ),
     );
   }
 
   Widget _getSearchBox() {
-    return Container(
-      height: 46,
-      margin: EdgeInsets.only(left: 18, right: 18, top: 12),
-      decoration: BoxDecoration(
-        color: backSearchBox,
-        borderRadius: BorderRadius.all(
-          Radius.circular(13),
+    return SliverToBoxAdapter(
+      child: Container(
+        height: 46,
+        margin: EdgeInsets.only(left: 18, right: 18, top: 12),
+        decoration: BoxDecoration(
+          color: backSearchBox,
+          borderRadius: BorderRadius.all(
+            Radius.circular(13),
+          ),
         ),
-      ),
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10),
-        child: Row(
-          children: [
-            Image.asset('images/icon_search.png'),
-            SizedBox(width: 8),
-            Expanded(
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Search User',
-                  hintStyle: TextStyle(color: whiteColor),
-                  enabledBorder: InputBorder.none,
-                  focusedBorder: InputBorder.none,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Row(
+            children: [
+              Image.asset('images/icon_search.png'),
+              SizedBox(width: 8),
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Search User',
+                    hintStyle: TextStyle(color: whiteColor),
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                  ),
                 ),
               ),
-            ),
-            Image.asset('images/icon_scan.png')
-          ],
+              Image.asset('images/icon_scan.png')
+            ],
+          ),
         ),
       ),
     );
