651e0001d324fc8b65ea35f43b52b0900046bb7d
MehrdadMoradi001 <moradi.mehrdad001@gmail.com>
2023-01-02 22:13:58 +0330

    1- creating Box Bottom

--- lib/screens/add_content_screen.dart
+++ lib/screens/add_content_screen.dart
@@ -10,52 +10,107 @@ class AddContentScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: deepColor,
       body: SafeArea(
-        child: _getWholeScrollingPart(),
+        child: Stack(
+          alignment: AlignmentDirectional.bottomCenter,
+          children: [
+            _getWholeScrollingPart(),
+            _getBoxBottom(),
+          ],
+        ),
       ),
     );
   }
 
   Widget _getWholeScrollingPart() {
     return CustomScrollView(
-        slivers: [
-          _getHeaderSection(),
-          _getSelectedImageContainer(),
-          _getGalleryImagesScrollGrid(),
-        ],
-      );
+      slivers: [
+        _getHeaderSection(),
+        _getSelectedImageContainer(),
+        _getGalleryImagesScrollGrid(),
+        SliverPadding(padding: EdgeInsets.only(bottom: 83)),
+      ],
+    );
   }
 
-  Widget _getGalleryImagesScrollGrid() {
-    return SliverPadding(
-            padding: EdgeInsets.symmetric(horizontal: 18,vertical: 10),
-            sliver: SliverGrid(
-              delegate: SliverChildBuilderDelegate(
-                childCount: 9,
-                (context, index) => ClipRRect(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
-                  ),
-                  child: FittedBox(
-                    fit: BoxFit.cover,
-                    child: Image.asset(
-                      'images/item$index.png',
+  Widget _getBoxBottom() {
+    return Container(
+            height: 83,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: backSearchBox,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(15),
+                topRight: Radius.circular(15),
+              ),
+            ),
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 18,vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Draft',
+                    style: TextStyle(
+                      fontFamily: 'GB',
+                      fontSize: 16,
+                      color: whiteColor
                     ),
                   ),
-                ),
-              ),
-              gridDelegate: SliverQuiltedGridDelegate(
-                crossAxisCount: 3,
-                mainAxisSpacing: 5,
-                crossAxisSpacing: 5,
-                repeatPattern: QuiltedGridRepeatPattern.mirrored,
-                pattern: [
-                  QuiltedGridTile(1, 1),
-                  QuiltedGridTile(1, 1),
-                  QuiltedGridTile(1, 1),
+                  Text(
+                    'Gallery',
+                    style: TextStyle(
+                      fontFamily: 'GB',
+                      fontSize: 16,
+                      color: whiteColor
+                    ),
+                  ),
+                  Text(
+                    'Take',
+                    style: TextStyle(
+                      fontFamily: 'GB',
+                      fontSize: 16,
+                      color: whiteColor
+                    ),
+                  ),
                 ],
               ),
             ),
           );
+  }
+
+
+  Widget _getGalleryImagesScrollGrid() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 18, vertical: 10),
+      sliver: SliverGrid(
+        delegate: SliverChildBuilderDelegate(
+          childCount: 10,
+          (context, index) => ClipRRect(
+            borderRadius: BorderRadius.all(
+              Radius.circular(10),
+            ),
+            child: FittedBox(
+              fit: BoxFit.cover,
+              child: Image.asset(
+                'images/item$index.png',
+              ),
+            ),
+          ),
+        ),
+        gridDelegate: SliverQuiltedGridDelegate(
+          crossAxisCount: 3,
+          mainAxisSpacing: 5,
+          crossAxisSpacing: 5,
+          repeatPattern: QuiltedGridRepeatPattern.mirrored,
+          pattern: [
+            QuiltedGridTile(1, 1),
+            QuiltedGridTile(1, 1),
+            QuiltedGridTile(1, 1),
+          ],
+        ),
+      ),
+    );
   }
 
   Widget _getSelectedImageContainer() {
