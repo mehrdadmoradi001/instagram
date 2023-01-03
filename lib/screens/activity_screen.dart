c1082c88f98dd60c1fba1fb39b27d0bfc2d2f54b
MehrdadMoradi001 <moradi.mehrdad001@gmail.com>
2023-01-03 14:51:24 +0330

    1- creating List Items 2- creating  shape: BoxShape.circle,

--- lib/screens/activity_screen.dart
+++ lib/screens/activity_screen.dart
@@ -55,7 +55,7 @@ class _ActivityScreenState extends State<ActivityScreen>
                     slivers: [
                       SliverList(
                         delegate: SliverChildBuilderDelegate(
-                          (context, index) => Text('Following'),
+                          (context, index) => _getRow(),
                           childCount: 100,
                         ),
                       ),
@@ -76,6 +76,99 @@ class _ActivityScreenState extends State<ActivityScreen>
             )
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _getRow() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 20, horizontal: 18),
+      child: Row(
+        children: [
+          Container(
+            height: 6,
+            width: 6,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: pinkColor,
+            ),
+          ),
+          SizedBox(width: 7),
+          SizedBox(
+            width: 40,
+            height: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+              child: FittedBox(
+                fit: BoxFit.cover,
+                child: Image.asset('images/phototest.jpg'),
+              ),
+            ),
+          ),
+          SizedBox(width: 10),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    'MehrdadMoradi001',
+                    style: TextStyle(
+                        fontFamily: 'GB', fontSize: 12, color: whiteColor),
+                  ),
+                  SizedBox(width: 5),
+                  Text(
+                    'Started ',
+                    style: TextStyle(
+                        fontFamily: 'GM', fontSize: 12, color: grayColor),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Text(
+                    'you',
+                    style: TextStyle(
+                        fontFamily: 'GM', fontSize: 12, color: grayColor),
+                  ),
+                  SizedBox(width: 8),
+                  Text(
+                    '3min',
+                    style: TextStyle(
+                        fontFamily: 'GB', fontSize: 10, color: grayColor),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Spacer(),
+          OutlinedButton(
+            onPressed: () {},
+            child: Text(
+              'Message',
+              style:
+                  TextStyle(fontSize: 12, fontFamily: 'GB', color: grayColor),
+            ),
+            style: OutlinedButton.styleFrom(
+              side: BorderSide(color: grayColor, width: 2),
+            ),
+          ),
+          // SizedBox(
+          //   width: 40,
+          //   height: 40,
+          //   child: ClipRRect(
+          //     borderRadius: BorderRadius.all(
+          //       Radius.circular(10),
+          //     ),
+          //     child: FittedBox(
+          //       fit: BoxFit.cover,
+          //       child: Image.asset('images/item1.png'),
+          //     ),
+          //   ),
+          // ),
+        ],
       ),
     );
   }
